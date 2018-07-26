5c2abeea0f709cb5d95ab499d397280acd4cc25f
salvatoremacbook <salvatoref1996@gmail.com>
2018-07-26 17:36:03 +0200

    Initial Commit

---
+++ MTU/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MTU
+//
+//  Created by Salvatore Fadda on 26/07/18.
+//  Copyright © 2018 Salvatore Fadda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
