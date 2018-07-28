7a84be782aa429004719a7712ffc746835e2f009
salvatoremacbook <salvatoref1996@gmail.com>
2018-07-28 20:40:17 +0200

    bfs function

--- MTU/main.c
+++ MTU/main.c
@@ -12,7 +12,9 @@
 
 #define MAX 100
 #define MAXS 2000
-#define START 50
+#define START 400
+#define LAST 1999
+
 
 //-----global declaration
 
@@ -42,11 +44,14 @@ typedef struct bfsNode {
     
     char tape[MAXS];
     char *head;
+    int curr;
     int currState;
-    struct bfsNode *pre;
+    struct bfsNode *next;
 
     
 } nodeBfs;
+
+typedef nodeBfs* nodeList;
 
 int max;
 
@@ -54,11 +59,13 @@ int acc[MAX];
 
 char *line;
 
+char *inputTape;
+
 nodeGraph *arrayNode[MAX];
 
 //-----prototype
 
-void spaceEater(void);
+int spaceEater(void);
 
 void graphBuilder(void);
 
@@ -67,6 +74,8 @@ int charParser(char *string, int n);
 char bfsFun(void);
 
 void addNode(int node);
+
+void addCoda(nodeList *lista, nodeList *pre, char tape[], char *head, int state, int curr);
 
 //-----main
 
@@ -109,14 +118,24 @@ int main(void) {
         spaceEater();
         if(strcmp(line,"run")){
             
-            spaceEater();
             char res;
-            while(!strcmp(line,"\n")){
+            int end = 0;
+            while(end == 0){
                 
-            
+                end = spaceEater();
+                //sostituire in maniera che la tape diventi dinamica
+                inputTape = (char*)malloc(sizeof(char)*MAXS);
+                for(int i = 0; i < MAXS; i++){
+                    
+                    inputTape[i] = '_';
+                }
+                for(int i = 0; (i+START) < MAXS; i++){
+                    
+                    inputTape[START] = line[i];
+                }
+                //------------------------------------------------
                 res = bfsFun();
                 printf("%c \n", res);
-                spaceEater();
                 
             }
             
@@ -133,22 +152,26 @@ int main(void) {
 //-----function
 
 //function that clar from the read line space and if the line it's void it jump from the first useful line
-void spaceEater(){
+int spaceEater(){
     
     free(line);
     line = (char*)malloc(sizeof(char)*MAXS);
     char i;
     int c = 0;
     scanf("%c", &i);
-    while(i != '\n' /*|| c == 0*/){
+    while(i != '\n' || c == 0){
         
         if(i != ' ' && i != '\n'){
             line[c] = i;
             c++;
         }
         scanf("%c", &i);
+        if(i == EOF){
+            return 1;
+        }
         
     }
+    return 0;
     
 }
 
@@ -274,17 +297,97 @@ void graphBuilder(){
 
 char bfsFun(){
     
-    char res;
-    int c = 0;
-    while(line[c] != '\0'){
+    char res = 'N';
+    int c = START;
+    int level = 0;
+    int stop = 0;
+    char newTape[MAXS];
+    char *newHead;
+    arcGraph *arc;
+    nodeList headNode2 = NULL;
+    nodeList currNode2 = NULL;
+    nodeList preNode1 = NULL;
+    nodeList currNode1;
+    //root
+    nodeBfs *node;
+    node = malloc(sizeof(nodeBfs));
+    node->currState = 0;
+    node->tape[c] = inputTape[c];
+    node->curr = c;
+    node->head = &(node->tape[c]);
+    node->next = NULL;
+    currNode1 = node;
+    while(level <= max){
         
+        while(currNode1 == NULL){
         
+            int accett = 0;
+            stop = arrayNode[currNode1->currState]->lastInsert + 1;
+        
+            for(int i = 0; i < stop; i++){
+            
+                arc = arrayNode[currNode1->currState]->extArc[i];
+                if(arc->toRead == *(currNode1->head)){
+                
+                    accett = 1;
+                    
+                    for(int i = 0; i < MAXS; i++){
+                    
+                        newTape[i] = currNode1->tape[i];
+                    }
+                    c = currNode1->curr;
+                    newTape[c] = arc->toWrite;
+                    if(arc->move == 'R'){
+                    
+                        c = c+1;
+                        newHead = &newTape[c];
+                    
+                    }else if(arc->move == 'L'){
+                    
+                        c = c-1;
+                        newHead = &newTape[c];
+                    
+                    }else{
+                    
+                        newHead = &newTape[c];
+                    
+                    }
+                
+                addCoda( &currNode2, &headNode2/*deve puntare alla testa della lista*/, newTape, newHead, arc->last->numberState, c);
+                
+                }
+            }
+            if(accett == 0){
+                for(int i = 0; i < MAX; i++){
+                    if(currNode1->currState == acc[i]){
+                        res = '1';
+                        return res;
+                    }
+                }
+            }
+            
+        
+            preNode1 = currNode1;
+            currNode1 = currNode1->next;
+            free(preNode1);
+        
+        }
+        if(headNode2 == NULL){
+            res = '0';
+            return res;
+        }
+        headNode2 = currNode1;
+        preNode1 = NULL;
+        currNode2 = NULL;
+        level++;
         
         
         
         
     }
-    
+    if(level <= max){
+        res = 'U';
+    }
     
     
     return res;
@@ -292,7 +395,18 @@ char bfsFun(){
 
 
 
-
+void addCoda(nodeList *next, nodeList *pre, char tape[], char *head, int state, int curr){
+    
+    if (*next == NULL) {
+        
+        
+    }
+    else {
+        addCoda(&((*lista)->next), )
+    }
+    
+    
+}
 
 
 
